aa75b2d54e8e14b72bad1cb9e945bcac05b68436
louisrankin <louis.rankin0@gmail.com>
2019-10-25 22:08:44 +1100

    Added host variable to dash app

--- demo.py
+++ demo.py
@@ -1012,4 +1012,4 @@ def update_image_src(value):
 
     return src_image, imageId
 
-app.run_server(debug=True, port=8056, use_reloader=False, threaded=False )
+app.run_server(debug=True, port=8056, use_reloader=False, threaded=False, , host='0.0.0.0' )
